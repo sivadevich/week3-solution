748f1cd78c653e21b4cd9eb2ccc9e2f8c0f9565f
sivadevi.ch <sivadevi.ch@gmail.com>
2018-03-19 14:45:13 -0400

    's'

--- app.js
+++ app.js
@@ -1,81 +1,80 @@
 (function (){
   'use strict' ;
-  angular.module('ShoppingListCheckOff', [])
+  angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+.directive('foundItems', FoundItemsDirective);
 
-.controller('ToBuyController', ToBuyController)
-.controller('AlreadyBoughtController', AlreadyBoughtController)
-.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
-
-ToBuyController.$inject = ['$scope','ShoppingListCheckOffService'];
-AlreadyBoughtController.$inject = ['$scope','ShoppingListCheckOffService'];
-
-ToBuyController.$inject = ['ShoppingListCheckOffService'];
-function ToBuyController(ShoppingListCheckOffService) {
-  var toBuyCntrl = this;
-
-  toBuyCntrl.items = ShoppingListCheckOffService.getItems();
-
-  toBuyCntrl.moveItemsToBought = function (itemIndex) {
-    console.log("Move Items To Bought");
-    ShoppingListCheckOffService.moveItemsToBought(itemIndex);
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      items: '<',
+     myTitle: '@title',
+     badRemove: '=',
+     onRemove: '&'
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: 'narrowCntrl',
+    bindToController: true
   };
+  return ddo;
+}
+
+function FoundItemsDirectiveController() {
+var narrowCntrl = this;
+//narrowCntrl.foundItemsList=[];
+}
+
+NarrowItDownController.$inject = ['$scope','MenuSearchService'];
+
+function NarrowItDownController($scope,MenuSearchService) {
+  var narrowCntrl = this;
+  narrowCntrl.searchText ="";
+  narrowCntrl.foundItemsList = [];
+  narrowCntrl.title =  " items )";
+  console.log("Search Text Keyed in "+narrowCntrl.searchText);
+  narrowCntrl.getMatchedMenuItems = function () {
+    console.log("narrowCntrl getMatchedItems search Text - "+narrowCntrl.searchText);
+    var result =  MenuSearchService.getMatchedMenuItems(narrowCntrl.searchText);
+    result.then(function (result) {
+        // process result and only keep items that match
+        for (var i=0; i<result.data.menu_items.length; i++) {
+            var description = result.data.menu_items[i].description;
+            if(description.search(narrowCntrl.searchText)!=-1){
+            narrowCntrl.foundItemsList.push(result.data.menu_items[i]);
+            }
+        }
+          console.log("narrowCntrl Length ### ****  - "+narrowCntrl.foundItemsList.length);
+        //console.log("Items found - "+narrowCntrl.foundItemsList.length);
+      //  return service.getFoundItems();
+      });
+//narrowCntrl.foundItemsList =MenuSearchService.getFoundItems();
+    //console.log("narrowCntrl Length - "+narrowCntrl.foundItemsList);
+    setTimeout(function () {
+           //deferred.resolve();
+          console.log("narrowCntrl Length ****  - "+narrowCntrl.foundItemsList.length);
+       }, 2000);
+  };
+  console.log("narrowCntrl Length ())))  - "+narrowCntrl.foundItemsList.length);
+
 
 }
 
-AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-function AlreadyBoughtController(ShoppingListCheckOffService) {
-  var alreadyBoughtCntrl = this;
-  alreadyBoughtCntrl.itemsBought = ShoppingListCheckOffService.getItemsBought();
-}
-
-  function ShoppingListCheckOffService() {
-    var service = this;
-    var itemsBoughtFlag =false;
-   // List of shopping items
-   var items = [];
-   var itemsBought =[];
-   var item1 = {
-     name: "Cookies",
-     quantity: "10"
-   };
-
-   var item2 = {
-     name: "Chocolates",
-     quantity: "20"
-   };
-
-   var item3 = {
-     name: "Drinks",
-     quantity: "30"
-   };
-   var item4 = {
-     name: "Chips",
-     quantity: "10"
-   };
-   var item5 = {
-     name: "ICeCreams",
-     quantity: "40"
-   };
-
-   items.push(item1);
-   items.push(item2);
-   items.push(item3);
-   items.push(item4);
-   items.push(item5);
-
-   service.getItems = function () {
-     return items;
-   };
-
-   service.getItemsBought = function () {
-     return itemsBought;
-   };
-   service.moveItemsToBought = function (itemIdex) {
-        itemsBought.push(items[itemIdex]);
-        items.splice(itemIdex, 1);
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+var foundItemsList = [];
+service.getFoundItems = function () {
+   return foundItemsList;
  };
+  service.getMatchedMenuItems = function (searchText) {
+    console.log("Search Text - "+searchText);
+    return $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    })
+    };
 }
-
-
-
 })();
